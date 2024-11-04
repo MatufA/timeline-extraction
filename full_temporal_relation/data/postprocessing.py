cff93dfb09cc3d5590a80135513d82c38410187f
adiel <amatuf@tremorvideo.com>
2024-11-04 19:42:49 +0200

    create PREPROCESSING AND POSTPROCESSING

--- full_temporal_relation/data/postprocessing.py
+++ full_temporal_relation/data/postprocessing.py
@@ -1,37 +1,52 @@
 import logging
 import re
+from pathlib import Path
+
 import pandas as pd
 
-from full_temporal_relation.data.preprocessing import Doc
+from full_temporal_relation.data.preprocessing import Doc, load_data
+from full_temporal_relation.metrics import get_n_graph_cycles
 
 
 def prepare_df_from_response(response: str, doc_obj: Doc):
+    supported_relations = ('before', 'after', 'equal', 'vague')
     data = []
     for line in response.strip().splitlines():
         e1e2_opt = re.search(r'e.*', line)
         if e1e2_opt is None:
             continue
-        e1e2 = e1e2_opt.group(0)
-        e1e2_lower = e1e2.strip().lower()
-        if 'before' in e1e2_lower:
-            relation = 'before'
-        elif 'after' in e1e2_lower:
-            relation = 'after'
-        elif 'equal' in e1e2_lower:
-            relation = 'equal'
-        elif 'vague' in e1e2_lower:
-            relation = 'vague'
-        else:
-            continue
 
-        split_e1_e2 = e1e2.split(f' {relation} ')
-        if len(split_e1_e2) == 1:
-            split_e1_e2 = e1e2.split(f' {relation.upper()} ')
+        e1e2 = e1e2_opt.group(0).strip()
 
-        if len(split_e1_e2) != 2:
+        regex_parsers = [r'(e\d+):\w+\s+(\w+)\s+(e\d+).*',
+                         r'(e\d+)\s+(\w+)\s+(e\d+).*',
+                         r'(e\d+)\s+\w+\s+(\w+)\s+(e\d+).*',
+                         r'(e\d+):(\w+)\s+(e\d+).*',
+                         r'(e\[\d+\])\s+(\w+)\s+(e\[\d+\]).*',
+                         r'(e\d+)\s+\[\w+\]\s+(e\d+):\s+(\w+)',
+                         r'(e\d+)\s+\[(\w+)\]\s+(e\d+)'
+                         ]
+
+        e1_relation_e2 = None
+        for regex_parser in regex_parsers:
+            e1_relation_e2 = re.search(regex_parser, e1e2.lower())
+            if e1_relation_e2 is not None:
+                break
+
+        if e1_relation_e2 is None:
             logging.error(f'unable to parse line: "{line}", ignored line')
             continue
-        e1, e2 = split_e1_e2
+
+        e1, relation, e2 = e1_relation_e2.groups()
+        e1 = e1.strip()
+        e2 = e2.strip()
+        relation = relation.strip()
+
+        if relation not in supported_relations:
+            if e2 in supported_relations:
+                e2, relation= relation, e2
+            else:
+                continue
 
         if e1 not in doc_obj.mapping or e2 not in doc_obj.mapping:
             unique_id = 'UNDEFINED'
@@ -45,7 +60,10 @@ def prepare_df_from_response(response: str, doc_obj: Doc):
         if relation == 'after':
             verb1, verb2 = verb2, verb1
             e1, e2 = e2, e1
+            label = 'after'
             relation = 'before'
+        else:
+            label = relation
 
         data.append({
             'docid': doc_obj.docid,
@@ -54,7 +72,8 @@ def prepare_df_from_response(response: str, doc_obj: Doc):
             'eiid1': e1.strip(),
             'eiid2': e2.strip(),
             'relation': relation,
-            'unique_id': unique_id
+            'unique_id': unique_id,
+            'p_label': label.upper()
         })
     return pd.DataFrame(data)
 
@@ -65,3 +84,57 @@ def transform_to_before(df, doc_id):
         lambda row: (row.verb2, row.verb1, row.eiid2, row.eiid1, 'BEFORE') if row.relation == 'AFTER' else (
             row.verb1, row.verb2, row.eiid1, row.eiid2, row.relation), axis='columns', result_type="expand")
     return doc_events
+
+
+def majority_vote_decision(parsed_response_path: Path, gold_data_path: Path, results_path: Path, min_votes: int = 3):
+    predicted_df = pd.read_csv(parsed_response_path)
+    # gold_before_only_df = load_data(gold_data_path)
+    # gold_df = load_data(gold_data_path)
+
+    # eiid1_eiid2 = list(zip(gold_before_only_df['eiid1'], gold_before_only_df['eiid2']))
+    # gold_before_only_df['unique_id'] = ['-'.join(sorted([eiid1, eiid2])) for eiid1, eiid2 in eiid1_eiid2]
+
+    predicted_df['score'] = 1
+    predicted_sum_df = predicted_df.groupby(['docid', 'unique_id', 'relation'])['score'].sum().reset_index()
+    predicted_sum_df = predicted_sum_df[predicted_sum_df['score'] > min_votes]
+
+    agg_preds = []
+    for (docid, unique_id), group in predicted_sum_df.groupby(['docid', 'unique_id']):
+        max_score = group.score.max()
+        relations = group[group.score == max_score]['relation'].to_list()
+
+        if len(relations) > 1:
+            agg_preds.append({
+                'docid': docid,
+                'unique_id': unique_id,
+                'relation': 'VAGUE',
+                'max_score': max_score,
+                'conflict_rel': ','.join(relations)
+            })
+        else:
+            agg_preds.append({
+                'docid': docid,
+                'unique_id': unique_id,
+                'relation': relations[0].upper(),
+                'max_score': max_score
+            })
+
+    agg_pred_df = pd.DataFrame(agg_preds)
+    predicted_df.relation = predicted_df.relation.str.upper()
+    merge_cols = ['docid', 'unique_id', 'relation']
+    predicted_agg_df = (pd.merge(predicted_df, agg_pred_df, on=merge_cols, how='inner')
+                        .drop_duplicates(merge_cols))
+
+    cycles = get_n_graph_cycles(predicted_agg_df)
+
+    cycles_df = pd.DataFrame.from_dict(cycles, orient='index', columns=['ccr']).reset_index()
+    cycles_df.columns = ['docid', 'n_cycles']
+
+    final_df = pd.merge(predicted_df, agg_pred_df,
+                        on=['docid', 'unique_id'],
+                        suffixes=(None, "_selected"),
+                        how='left')
+    final_df = pd.merge(final_df, cycles_df, on=['docid'], how='left')
+    final_df['min_vote'] = min_votes
+
+    final_df.to_csv(results_path, index=False)
--- full_temporal_relation/data/preprocessing.py
+++ full_temporal_relation/data/preprocessing.py
@@ -1,5 +1,8 @@
+import logging
+import re
 from typing import Union
 
+import numpy as np
 import pandas as pd
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -8,15 +11,77 @@ from bs4 import BeautifulSoup
 def load_data(paht: Union[str, Path]) -> pd.DataFrame:
     df = pd.read_csv(paht, sep='\t', header=None,
                      names=['docid', 'verb1', 'verb2', 'eiid1', 'eiid2', 'relation'])
-    df.eiid1 = 'e' + df.eiid1.astype(str)
-    df.eiid2 = 'e' + df.eiid2.astype(str)
+    df.eiid1 = 'ei' + df.eiid1.astype(str)
+    df.eiid2 = 'ei' + df.eiid2.astype(str)
 
     mask = df['relation'] == 'after'
     df.loc[mask, ['verb1', 'verb2']] = df.loc[mask, ['verb2', 'verb1']].values
+    df['label'] = df['relation'].copy()
     df['relation'] = df['relation'].replace('AFTER', 'BEFORE')
+
+    eiid1_eiid2 = list(zip(df['eiid1'], df['eiid2']))
+    df['unique_id'] = ['-'.join(sorted([eiid1, eiid2])) for eiid1, eiid2 in eiid1_eiid2]
 
     return df
 
+def generate_training_dataset(gold_df: pd.DataFrame, docs_dir: Union[str, Path]) -> pd.DataFrame:
+    docs = [Doc(path) for path in Path(docs_dir).glob('*.tml')]
+
+    doc_relations = []
+    for doc in docs:
+        doc_relation = gold_df[gold_df['docid'] == doc.docid].copy()
+        doc_relation['text'] = pd.NA
+
+        # doc.get_text_with_relation_external()
+        lines = doc.get_text().split('\n\n')
+
+        event_dict = {}
+        for idx, line in enumerate(lines):
+            ids_groups = re.findall(r'(ei\d+):\w+\s*', line)
+            if ids_groups:
+                indexes = np.repeat(idx, len(ids_groups))
+                event_dict.update(dict(zip(ids_groups, indexes)))
+
+        plain_text_lines = re.sub(r'ei\d+:(\w+)\s*', r'\1 ', doc.get_text()).split('\n\n')
+
+        for idx, row in doc_relation.iterrows():
+            if row['eiid1'] not in event_dict:
+                logging.warning(f'docid: {doc.docid} and eid: {row["eiid1"]}')
+                continue
+
+            if row['eiid2'] not in event_dict:
+                logging.warning(f'docid: {doc.docid} and eid: {row["eiid2"]}')
+                continue
+
+            e1_index = event_dict[row['eiid1']]
+            e2_index = event_dict[row['eiid2']]
+
+            if e1_index != e2_index:
+                min_idx = min(e1_index, e2_index)
+                max_idx = max(e1_index, e2_index)
+
+                doc_relation.at[idx, 'text'] = '\n'.join(plain_text_lines[min_idx: max_idx+1])
+            else:
+                doc_relation.at[idx, 'text'] = plain_text_lines[e1_index]
+
+        # prev_idx = 0
+        # for idx in range(2, len(lines) + 1):
+        #     tuple_lines = lines[prev_idx:idx]
+        #     text = '\n'.join(tuple_lines)
+        #
+        #     prev_idx = idx - 1
+        #
+        #     ids_groups = re.findall(r'(e\d+):\w+\s+', text)
+        #     if len(ids_groups) < 2:
+        #         continue
+        #
+        #     mask = doc_relation.eiid1.isin(ids_groups) & doc_relation.eiid2.isin(ids_groups) & doc_relation.text.isna()
+        #     if not doc_relation[mask].empty:
+        #         doc_relation.loc[mask, 'text'] = text
+
+        doc_relations.append(doc_relation)
+
+    return pd.concat(doc_relations, ignore_index=True).dropna(subset='text')
 
 class Doc:
     def __init__(self, path: Path):
@@ -33,12 +98,12 @@ class Doc:
         return BeautifulSoup(data, features='lxml')
 
     def get_mapping(self):
-        text = self.doc.find(name="text")
-        return {d.get("eid"): d.get_text() for d in text.find_all('event') if d.get("eid") is not None}
+        # text = self.doc.find(name="MAKEINSTANCE")
+        return {d.get("eventid"): d.get("eiid") for d in self.doc.find_all('makeinstance')}
 
     def get_text(self):
         text = self.doc.find(name="text")
-        [d.replace_with(f'{d.get("eid")}:{d.get_text()}') for d in text.find_all('event')]
+        [d.replace_with(f'{self.mapping[d.get("eid")] if d.get("eid") in self.mapping else d.get("eid")}:{d.get_text()}')  for d in text.find_all('event')]
         [d.replace_with(d.get_text()) for d in text.find_all('timex3')]
         return text.get_text().strip()
 
@@ -54,3 +119,19 @@ class Doc:
     The text-
     {self.get_text()}
         """
+
+if __name__ == '__main__':
+    DATA_PATH = Path('../../data')
+    MATRES_PATH = DATA_PATH / 'MATRES'
+    DOCS_DIRS_PATH = MATRES_PATH / 'raw' / 'TBAQ-cleaned'
+
+    BASE_NAMES = ['AQUAINT', 'TimeBank', 'te3-platinum']
+    BASE_DF_PATHS = ['aquaint.txt', 'timebank.txt', 'platinum.txt']
+
+    for name, df_name in zip(BASE_NAMES, BASE_DF_PATHS):
+        gold_df = load_data(MATRES_PATH / df_name)
+
+        df = generate_training_dataset(gold_df, docs_dir=DOCS_DIRS_PATH / name)
+        output = DATA_PATH / 'trc-prepared-data'
+        output.mkdir(exist_ok=True)
+        df.to_csv(output / f'{name}.csv', index=False)
