a5413daa49dc330a7ca4e27146654a9534f7f59e
MatufA <matufadiel@gmail.com>
2024-12-26 11:20:08 +0200

    support vague format and fit pipeline to new llm format response

--- full_temporal_relation/pipeline.py
+++ full_temporal_relation/pipeline.py
@@ -9,12 +9,12 @@ import torch
 import gc
 from dotenv import load_dotenv
 
-from full_temporal_relation.data.postprocessing import prepare_df_from_response, majority_vote_decision, prepare_df_from_json_response, majority_vote_decision_new
+from full_temporal_relation.data.postprocessing import majority_vote_decision_new
 from full_temporal_relation.data.preprocessing import Doc, load_data
 from full_temporal_relation.metrics import summary_results
 from full_temporal_relation.models.HuggingFaceClient import HuggingfaceClient
 from full_temporal_relation.models.OpenAIClient import OpenAIClient
-from full_temporal_relation.models.LLModel import LLModel
+from full_temporal_relation.models.LLModel import LLModel, LabelParser, JsonParser
 # from full_temporal_relation.models.TogetherAIClient import TogetherAIClient
 # from full_temporal_relation.models.gemini import Gemini
 # from full_temporal_relation.models.llama3 import GroqModel
@@ -29,26 +29,34 @@ gold_data_path = MATRES_DATA_PATH / 'platinum.txt'
 TRC_RAW_PATH = DATA_PATH / 'TRC'
 TRC_RESULTS_PATH = TRC_RAW_PATH / 'results'
 
-def generate_suffix_name(model_name: str, method: str, suffix_path: str):
+def generate_suffix_name(model_name: str, method: str, suffix_path: str, use_vague: bool = False):
     suffixes = [model_name.split('/')[1] if '/' in model_name else model_name, method]
+    if use_vague:
+         suffixes.append('w_vague')
     if suffix_path:
         suffixes.append(suffix_path)
     return '-'.join(suffixes)
 
 def main(model_name: str, method: str, model: LLModel,  prompt_params: List[str],
-         raw_text_name: str, data_name: str, prompt: Prompt, suffix_path: str = '', mode: str = 'multi'):
-    suffix_name = generate_suffix_name(model_name, method, suffix_path)
+         raw_text_name: str, data_name: str, prompt: Prompt, suffix_path: str = '',
+         mode: str = 'multi', overwrite: bool = False, use_vague: bool = False, 
+         skip_model_eval: bool = False):
+    suffix_name = generate_suffix_name(model_name, method, suffix_path, use_vague)
 
     llm_response_path = TRC_RAW_PATH / 'llm_response' / method / f'{mode}-{data_name}-{suffix_name}.jsonl'
     parsed_response_path = TRC_RAW_PATH / 'parsed_responses' / method / f'{mode}-{data_name}-results-{suffix_name}.csv'
     results_path = TRC_RAW_PATH / 'results' / method / f'{mode}-{data_name}-results-{suffix_name}.csv'
+    checkpoint_results_path = TRC_RAW_PATH / 'llm_raw_response' / method / f'{mode}-{data_name}-{suffix_name}.jsonl'
+    checkpoint_results_path.parent.mkdir(parents=True, exist_ok=True) 
 
     # Generate model and response
-    # resluts = model.generate_responses(text_path=TRC_RAW_PATH / 'raw_text' / raw_text_name,
-    #                         #  prompt_path=TRC_RAW_PATH / 'prompts' / method / prompt_filename,
-    #                         results_path=llm_response_path,
-    #                         prompt_params=prompt_params, 
-    #                         prompt_template=prompt)
+    if not skip_model_eval:
+        resluts = model.generate_responses(text_path=TRC_RAW_PATH / 'raw_text' / raw_text_name,
+                                results_path=llm_response_path,
+                                prompt_params=prompt_params, 
+                                prompt_template=prompt, 
+                                overwrite=overwrite, 
+                                checkpoint_results=checkpoint_results_path)
 
     # results_df = pd.DataFrame({'doc_id': res['doc_id'], 'trail': res['trail'], 'response': res['response']} for res in resluts)
 
@@ -66,12 +74,13 @@ def main(model_name: str, method: str, model: LLModel,  prompt_params: List[str]
     #     results_df = pd.read_json(llm_response_path, lines=True)
     results_df = pd.read_json(llm_response_path, lines=True)
 
+
     data = []
     for idx, row in results_df.iterrows():
         if isinstance(prompt, PairwisePrompt):
             true_labels = [row.true_label.upper()] if 'true_label' in row else ['NO_TRUE_LABEL']
         else:
-            true_labels = row.true_label.upper() if 'true_label' in row else 'NO_TRUE_LABEL'
+            true_labels = ['NO_TRUE_LABEL']
 
         if isinstance(row.response, str):
             p_label = row.response.upper()
@@ -119,14 +128,15 @@ def get_summary_results(model_name: str, method: str, labeled_path: Path, result
     result_file_suffix = generate_suffix_name(model_name, method, suffix_path)
 
     gold_df = load_data(labeled_path)
-    non_vague_gold_df = gold_df[gold_df['label'] != 'VAGUE']
+    # non_vague_gold_df = gold_df[gold_df['label'] != 'VAGUE']
+    non_vague_gold_df = gold_df
     results_df = pd.read_csv(results_path)
     results_df = pd.merge(results_df, non_vague_gold_df[['docid', 'unique_id']], how='inner',
                          on=['docid', 'unique_id']).drop_duplicates(['docid', 'unique_id'])
 
     df = summary_results(results_df,
                          non_vague_gold_df,
-                         model_name + '-matres')
+                         model_name)
     df['method'] = method
     df['suffix_path'] = suffix_path
     df['model_name'] = model_name
@@ -134,7 +144,7 @@ def get_summary_results(model_name: str, method: str, labeled_path: Path, result
 
 
 if __name__ == '__main__':
-    gpu_device = 2
+    gpu_device = 1
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_device)
     # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
     # os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'  # for mistralai
@@ -146,8 +156,8 @@ if __name__ == '__main__':
     labeled_df_name = 'platinum.txt'
 
     # mode = 'pair'
-    # mode = 'multi'
-    mode = 'comb'
+    mode = 'multi'
+    # mode = 'comb'
 
     methods = ['zero-shot', 'few-shot']
     # method = 'zero-shot'
@@ -182,20 +192,29 @@ if __name__ == '__main__':
     # model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
     # model = TogetherAIClient(model_name)
     
-    model_names = ["meta-llama/Llama-3.1-8B-Instruct", "meta-llama/Llama-3.2-3B-Instruct"] 
+    # model_names = ["meta-llama/Llama-3.1-8B-Instruct", "meta-llama/Llama-3.2-3B-Instruct"] 
     # model_names = ['mistralai/Mistral-7B-Instruct-v0.3']
 
     model_names = ['gpt-4o-mini'] 
 
+    use_vague = True
+
+    # parser_type = LabelParser
+    parser_type = JsonParser
+    overwrite=True
+
     for model_name in model_names:
-    # model_name = "meta-llama/Llama-3.2-3B-Instruct"
         for method in methods:
             is_few_shot = (method == 'few-shot')
-            # prompt = MultiEvents(use_few_shot=is_few_shot, use_vague=False, provide_justification=False)
-            prompt = PairwisePrompt(use_few_shot=is_few_shot, use_vague=False)
 
-            # model = HuggingfaceClient(model_name=model_name, device=gpu_device)
-            model = OpenAIClient(model_name=model_name)
+            if isinstance(parser_type, LabelParser):
+                prompt = PairwisePrompt(use_few_shot=is_few_shot, use_vague=use_vague)
+            else:
+                prompt = MultiEvents(use_few_shot=is_few_shot, use_vague=use_vague, provide_justification=False)
+            
+
+            # model = HuggingfaceClient(model_name=model_name, device=gpu_device, parser=parser_type)
+            model = OpenAIClient(model_name=model_name, use_formate=False, parser=parser_type)
 
             """
             train - timebank.txt
@@ -204,7 +223,8 @@ if __name__ == '__main__':
             """
 
             results_path = main(model_name, method, model, prompt_params,
-                raw_text_name=raw_text_name, suffix_path=suffix_path, data_name=name, mode=mode, prompt=prompt)
+                raw_text_name=raw_text_name, suffix_path=suffix_path, 
+                data_name=name, mode=mode, prompt=prompt, overwrite=overwrite, skip_model_eval=False)
 
             results_df = get_summary_results(model_name, method,
                                         labeled_path=MATRES_DATA_PATH / labeled_df_name,
@@ -215,8 +235,8 @@ if __name__ == '__main__':
             results_metrics_path.parent.mkdir(parents=True, exist_ok=True)
             results_df.to_csv(results_metrics_path, index=False)
             
-            # del model
-            # torch.cuda.empty_cache()
-            # gc.collect()
+            del model
+            torch.cuda.empty_cache()
+            gc.collect()
             print(f'writing results to {results_metrics_path}')
             print('-' * 50)
