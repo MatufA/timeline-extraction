f20c9c94297b89c839462f66743dbcc45e210e93
MatufA <matufadiel@gmail.com>
2025-01-02 22:05:40 +0200

    support cycles markers

--- full_temporal_relation/visualization/graph.py
+++ full_temporal_relation/visualization/graph.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def draw_directed_graph(graph, title=None, label_name=None):
+def draw_directed_graph(graph, title=None, label_name=None, cycles_only: bool = False):
     plt.rcParams["figure.figsize"] = (15, 10)
 
     labels = nx.get_node_attributes(graph, label_name)
@@ -24,21 +24,22 @@ def draw_directed_graph(graph, title=None, label_name=None):
     options = {"edgecolors": "tab:gray", "node_size": 800, "alpha": 0.9}
     nx.draw_networkx_nodes(graph, pos, node_color="tab:blue", **options)
     
+    if not cycles_only:
+        nx.draw_networkx_edges(
+            graph,
+            pos,
+            arrows=True,
+            edgelist=graph.edges(),
+            edge_color='tab:blue',
+            alpha=0.5,
+            width=3,
+            label="S",
+            arrowstyle='-|>')
+            
     if label_name is not None:
         nx.draw_networkx_labels(graph, pos, labels=labels, font_size=12, font_color="whitesmoke")
     else:
         nx.draw_networkx_labels(graph, pos, font_size=12, font_color="whitesmoke")
-
-    nx.draw_networkx_edges(
-        graph,
-        pos,
-        arrows=True,
-        edgelist=graph.edges(),
-        edge_color='tab:blue',
-        alpha=0.5,
-        width=3,
-        label="S",
-        arrowstyle='-|>')
     
     if cycles:
         nx.draw_networkx_edges(graph, pos, edgelist=cycles, edge_color="r", width=2)
