65c892191c19c474d11f1c0c8b060dbd641ec8b6
Adiel Meir <matufadiel@gmail.com>
2025-09-16 22:43:14 +0300

    Add visualization modules for timeline extraction: data plotting and directed graph drawing. Includes functions for plotting relation bars and drawing graphs with cycle detection and layout options.

---
+++ timeline_extraction/visualization/data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_relation_bars(df: pd.DataFrame, ax=None, title=""):
+    df_relation = df.relation.value_counts()
+    df_relation_dist = df_relation / df_relation.sum()
+
+    df_relation_dist.to_frame().plot(kind="bar", ax=ax)
+    for i, n in enumerate(df_relation_dist):
+        if ax:
+            ax.text(i, n / 2, f"{np.round(n, 3)}%", va="center", ha="center")
+            ax.set_title(title)
+        else:
+            plt.text(i, n / 2, f"{np.round(n, 3)}%", va="center", ha="center")
+    return df_relation.reset_index()
